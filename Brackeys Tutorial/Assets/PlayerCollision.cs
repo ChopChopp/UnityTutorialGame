cdede1e110b8363d694e4367a3e7224cb58c2d96
ChopChopp <ozturk_umut02@hotmail.com>
2019-08-17 18:00:17 +0200

    Initial commit

---
+++ Brackeys Tutorial/Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movementScript;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            movementScript.enabled = false;
+            Debug.Log("test");
+        }
+    }
+}
